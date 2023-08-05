885b31f751c818d177d2dbbe282a103a7504acd1
PolarBowler <f.windisch97@gmail.com>
2023-08-05 22:01:49 +0200

    cleanup formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<form action="https://www.google.com/search" method="get">
-    <input type="text" name="q">
-    <button type="submit">Submit</button>
+<form
+  action="https://www.google.com/search"
+  method="get"
+>
+  <input
+    type="text"
+    name="q"
+  />
+  <button type="submit">Submit</button>
 </form>
